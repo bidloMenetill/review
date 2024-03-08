e2d3c996b8ab61ddfda43f68a12e737ada5c767d
Kenjebai <kenjebai009@gmail.com>
2024-03-08 20:53:00 +0600

    translation

--- src/widgets/aboutUs/Team.jsx
+++ src/widgets/aboutUs/Team.jsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next";
+
 export const Team = () => {
+
+  const { t } = useTranslation()
   return (
     <div>
       <div className="bg-[url('/src/shared/img/team.png')] w-[1920px] h-[2800px]">
         <div className="w-[1719px] mx-auto">
-          <h2 className="text-[#F5F5F5] font-medium text-8xl leading-[122px]">Команда</h2>
+          <h2 className="text-[#F5F5F5] font-medium text-8xl leading-[122px]">{t('aboutUs.thirdSection.title')}</h2>
         </div>
       </div>
     </div>
--- src/widgets/aboutUs/WhyQRush.jsx
+++ src/widgets/aboutUs/WhyQRush.jsx
@@ -1,21 +1,25 @@
+import { useTranslation } from "react-i18next";
+
 export const WhyQRush = () => {
+
+  const { t } = useTranslation()
   return (
     <div>
       <div className="bg-[url('/src/shared/img/Why-Q-Rush.png')] w-[1920px] h-[802px] py-28">
         <div className="w-[1719px] mx-auto">
-          <h3 className="text-[#F5F5F5] text-center text-5xl leading-[61px] font-medium">Почему Q-Rush Studio?  </h3>
-          <div className="flex py-32">
+          <h3 className="text-[#F5F5F5] text-center text-5xl leading-[61px] font-medium">{t('aboutUs.secondSection.title')}</h3>
+          <div className="flex py-32 gap-2">
             <div className="text-center">
-              <h4 className="text-[#DC6441] text-3xl font-bold">Качество</h4>
-              <p className="text-[#F5F5F5] text-xl leading-10 font-medium">Q-Rush Studio  дает вам доступ к команде опытных и преданных своему делу преподавателей, помещениям и оборудованию, которые позволят вам достичь результатов, превосходящих ваши ожидания.</p>
+              <h4 className="text-[#DC6441] text-3xl font-bold">{t('aboutUs.secondSection.firstSection.title')}</h4>
+              <p className="text-[#F5F5F5] text-xl leading-10 font-medium">{t('aboutUs.secondSection.firstSection.upperTitle')}</p>
             </div>
             <div className="text-center">
-              <h4 className="text-[#DC6441] text-3xl font-bold">Разнообразие</h4>
-              <p className="text-[#F5F5F5] text-xl leading-10 font-medium">Q-Rush Studio  с гордостью принимает людей со всего мира, представителей всех слоев общества и разного происхождения. Учитесь в среде, где отмечаются различия и поощряется сотрудничество.</p>
+              <h4 className="text-[#DC6441] text-3xl font-bold">{t('aboutUs.secondSection.secondSection.title')}</h4>
+              <p className="text-[#F5F5F5] text-xl leading-10 font-medium">{t('aboutUs.secondSection.secondSection.upperTitle')}</p>
             </div>
             <div className="text-center">
-              <h4 className="text-[#DC6441] text-3xl font-bold">Страсть</h4>
-              <p className="text-[#F5F5F5] text-xl leading-10 font-medium">Практикуйтесь и развивайтесь вместе с людьми, которые, как и вы, инвестируют в свое искусство и творчество. Вы можете быть уверены: вашу страсть к звуку разделяют преподаватели, студенты и сотрудники Q-Rush Studio.</p>
+              <h4 className="text-[#DC6441] text-3xl font-bold">{t('aboutUs.secondSection.thirdSection.title')}</h4>
+              <p className="text-[#F5F5F5] text-xl leading-10 font-medium">{t('aboutUs.secondSection.thirdSection.upperTitle')}</p>
             </div>
           </div>
         </div>
