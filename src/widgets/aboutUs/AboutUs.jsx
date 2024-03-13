e33df9c1de6783c3ea26537d21ea277ed8731e82
Kenjebai <kenjebai009@gmail.com>
2024-03-14 00:12:20 +0600

    changed text

--- src/widgets/aboutUs/AboutUs.jsx
+++ src/widgets/aboutUs/AboutUs.jsx
@@ -10,19 +10,19 @@ export const AboutUs = () => {
   const { t } = useTranslation();
   return (
     <div>
-      <div className="bg-[url('/src/shared/img/about.png')] h-[974px] w-[1920px]">
+      <div className="bg-[url('/src/shared/img/about1.png')] h-[974px] w-[1920px]">
 
         <div className="flex justify-center items-center">
           <div className="py-40 flex flex-row gap-[76px] w-[1719px] items-center">
-            <div className="w-[938px] flex flex-col gap-6">
+            <div className="w-[1255px] flex flex-col gap-6">
               <h2 className="text-[#F5F5F5] font-medium text-8xl leading-[122px]">{t('aboutUs.firstSection.title')}</h2>
               <p className="text-[#F5F5F5] font-medium text-xl leading-10">
                 {t('aboutUs.firstSection.upperTitle')}
               </p>
             </div>
-            <div>
+            {/* <div>
               <img src={rectangle} alt="rectangle" />
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
