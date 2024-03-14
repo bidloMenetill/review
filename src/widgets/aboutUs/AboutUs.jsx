d39611d86fdec6e7cffd05860154251cd8f0fea8
Kenjebai <kenjebai009@gmail.com>
2024-03-14 23:47:03 +0600

    changed photos

--- src/widgets/aboutUs/AboutUs.jsx
+++ src/widgets/aboutUs/AboutUs.jsx
@@ -12,7 +12,7 @@ export const AboutUs = () => {
     <div>
       <div className="bg-[url('/src/shared/img/about1.png')] h-[960px] w-[1920px]">
 
-        <div className="flex justify-center items-center">
+        <div className="about flex justify-center items-center">
           <div className="py-40 flex flex-row gap-[76px] w-[1719px] items-center">
             <div className="w-[1255px] flex flex-col gap-6">
               <h2 className="text-[#F5F5F5] font-medium text-8xl leading-[122px]">{t('aboutUs.firstSection.title')}</h2>
