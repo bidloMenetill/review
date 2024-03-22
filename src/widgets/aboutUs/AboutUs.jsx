7ba05f827d7fc6b531e4e9cf6bfa2b2fc247ab18
Kenjebai <Kenjebai>
2024-03-22 17:19:32 +0600

    adaptive

--- src/widgets/aboutUs/AboutUs.jsx
+++ src/widgets/aboutUs/AboutUs.jsx
@@ -1,14 +1,24 @@
 import { useTranslation } from 'react-i18next';
+import { useMediaQuery } from '../../shared';
 
 export const AboutUs = () => {
   const { t } = useTranslation();
+  const isNotebook = useMediaQuery(
+    '(min-width: 386px) and (max-width: 1720px)'
+  );
   return (
     <div>
       <div className='bg-[url("/src/shared/img/about1.png")] h-[960px] w-[1920px]'>
         <div className='about pt-16 flex justify-center items-center'>
           <div className='py-40 flex flex-row gap-[76px] w-11/12 items-center'>
             <div className='w-8/12 flex flex-col gap-6'>
-              <h2 className='text-[#F5F5F5] font-medium text-8xl leading-[122px]'>
+              <h2
+                className={
+                  isNotebook
+                    ? 'font-normal text-7xl text-[#F5F5F5] pt-14 leading-[100px]'
+                    : 'pt-16 text-[#F5F5F5] font-medium text-8xl leading-[122px]'
+                }
+              >
                 {t('aboutUs.firstSection.title')}
               </h2>
               <p className='text-[#F5F5F5] font-medium text-xl leading-10'>
