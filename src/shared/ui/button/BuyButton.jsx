a5be382ad50c01ea8a1ee5a59ebb5d7ff4881dfa
Yryskeldi <kuralyrys@gmail.com>
2024-03-26 13:47:10 +0300

    did layout shopPage

--- src/shared/ui/button/BuyButton.jsx
+++ src/shared/ui/button/BuyButton.jsx
@@ -1,20 +1,6 @@
-import { useMediaQuery } from '../../hooks/useMediaQuery';
-
 export const BuyButton = () => {
-  const isWideScreen1 = useMediaQuery(
-    '(min-width: 1620px) and (max-width:1820px)'
-  );
-  const isWideScreen2 = useMediaQuery(
-    '(min-width: 1320px) and (max-width:1620px)'
-  );
-  const isWideScreen3 = useMediaQuery(
-    '(min-width: 1024px) and (max-width:1320px)'
-  );
-
   return (
-    <button
-      className={`mt-[21px] rounded-[40px] bg-[#000000] ${isWideScreen1 ? 'w-[200px] h-[50px]' : isWideScreen2 ? 'w-[120px] h-[40px]' : isWideScreen3 ? 'w-[100px] h-[40px]' : 'w-[260px] h-[50px]'}`}
-    >
+    <button className='mt-[21px] rounded-[40px] bg-[#f93822] w-[200px] lg:w-[300px] xl:w-[344px] h-[50px] lg:h-[70px]'>
       <a className='text-[#f5f5f5] text-[25px] font-montserrat' href='#'>
         Купить
       </a>
