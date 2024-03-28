8a22a5a360153286d23532eed1c2842f3c0c17f7
bidloMenetill <alinur.ali.07@mail.ru>
2024-03-28 21:42:14 +0600

    add backend axios

--- src/features/index.js
+++ src/features/index.js
@@ -1,5 +1,5 @@
 export { People } from './people/People';
-export { ChangeBgIntroduction } from './bgIntroduction/ChangeBgIntroduction';
+export { ChangeBgIntroduction } from './changeBgIntroduction/ChangeBgIntroduction';
 export { NavigationOrange } from './navigationOrange/NavigationOrange';
 export { Questions } from './questions/Questions';
 export { FormQuestion } from './formQuestion/FormQuestion';
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import { Routing } from './app/routes/Routing';
+
 import './i18n';
 import './app/styles/index.css';
 
-import { Routing } from './app/routes/Routing';
+axios.interceptors.request.use(
+  request => {
+    console.log('Starting Request:', request);
+    return request;
+  },
+  error => {
+    console.error('Request Error:', error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    console.log('Response:', response);
+    return response;
+  },
+  error => {
+    console.error('Response Error:', error);
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
