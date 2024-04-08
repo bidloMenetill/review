779ec05cbfba3986751e5a4c7f4445aebcac3413
Sultanbek <sultan.02173@gmail.com>
2024-04-08 22:58:56 +0600

    fix design in section rehearsalCost

--- src/widgets/rehearsalCostPageSections/rehearsalCostFirstSection/RehearsalCostFirstSection.jsx
+++ src/widgets/rehearsalCostPageSections/rehearsalCostFirstSection/RehearsalCostFirstSection.jsx
@@ -22,6 +22,138 @@ export const RehearsalCostFirstSection = () => {
     { id: 3, img: rectangle51 },
     { id: 4, img: rectangle51 },
   ];
+  const arr = [
+    {
+      id: 0,
+      title: 'Гитарные усилители (на выбор)',
+      firstDescription: '- Peavey bandit 112 (2шт)',
+      secondDescription: '- Marshall MG100DFX',
+      thirdDescription: '- Marshall Haze 40',
+      fourthDescription: ' Black Heart bh15-112',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 1,
+      title: 'Гитары (на выбор)',
+      firstDescription: '- Yamaha BB 5 string',
+      secondDescription: '- Washburn bass 5 string',
+      thirdDescription: '- ESP ec256 (форма LesPaul)',
+      fourthDescription: '- Ibanez RG',
+      fifthDescription: '- Vintage (реплика Gibson SG)',
+      sixthDescription: '- Простая китайская Акустика',
+    },
+    {
+      id: 2,
+      title: 'Микрофоны (на выбор)',
+      firstDescription: '- Shure SM57 (2 шт)',
+      secondDescription: '- AKG D5',
+      thirdDescription: '- Shure SM 58',
+      fourthDescription: '- tBone sm58',
+      fifthDescription: '- Lane (2 шт.)',
+      sixthDescription: '',
+    },
+    {
+      id: 3,
+      title: 'Пульт',
+      firstDescription: '- Yamaha MG 166cx',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 4,
+      title: 'Усилители',
+      firstDescription: '- Crown xls 2500',
+      secondDescription: '- Crown CDi 1000',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 5,
+      title: 'Басовый усилитель',
+      firstDescription: '- Ampeg BA 115',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 6,
+      title: 'Цифровое Фортепиано',
+      firstDescription: '- Yamaha Grand dgx 630',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 7,
+      title: 'Акуст система',
+      firstDescription: '- Alto elvis 15.2xl',
+      secondDescription: '- Сабы Electrovoice',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 8,
+      title: 'Триггер для бочки',
+      firstDescription: '- TrigMic 2 Laser Pick',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 9,
+      title: 'Триггер для бочки',
+      firstDescription: '- TrigMic 2 Laser Pick',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 10,
+      title: 'Триггер для бочки',
+      firstDescription: '- TrigMic 2 Laser Pick',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 9,
+      title: 'Триггер для бочки',
+      firstDescription: '- TrigMic 2 Laser Pick',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+    {
+      id: 10,
+      title: 'Триггер для бочки',
+      firstDescription: '- TrigMic 2 Laser Pick',
+      secondDescription: '',
+      thirdDescription: '',
+      fourthDescription: '',
+      fifthDescription: '',
+      sixthDescription: '',
+    },
+  ];
   const arrayEquipment = [
     [
       {
@@ -145,7 +277,7 @@ export const RehearsalCostFirstSection = () => {
         <h3 className='mt-[27px] mb-[20px] md:mt-[38px] md:mb-[32px] text-[20px] font-[600] xl:text-[30px] lg:text-[25px] tablet:text-[22px]'>
           Список оборудования
         </h3>
-        <div className='max-w-[1170px] mx-auto md:flex md:justify-between text-left xl:max-w-[1570px] lg:max-w-[1270px]'>
+        {/* <div className='max-w-[1170px] mx-auto md:flex md:justify-between text-left xl:max-w-[1570px] lg:max-w-[1270px]'>
           <section className='w-full md:flex md:justify-between gap-[146px]'>
             {mobileScreen && openList === false ? (
               <CardRehearsalSection
@@ -165,6 +297,19 @@ export const RehearsalCostFirstSection = () => {
               ))
             )}
           </section>
+        </div> */}
+        <div className='w-[1570px] mx-auto grid grid-cols-3 gap-4'>
+          {arr?.map(el => (
+            <ul key={el.id} className='text-left'>
+              <li className='mb-[10px] mt-[10px] text-[#fff]'>{el.title}</li>
+              <li>{el.firstDescription}</li>
+              <li>{el.secondDescription}</li>
+              <li>{el.thirdDescription}</li>
+              <li>{el.fourthDescription}</li>
+              <li>{el.fifthDescription}</li>
+              <li>{el.sixthDescription}</li>
+            </ul>
+          ))}
         </div>
         {mobileScreen && openList === false ? (
           <p
