655c16d102bf5deeb318934297e0bb057219826f
bidloMenetill <alinur.ali.07@mail.ru>
2024-04-16 23:34:20 +0600

    add zustand and create axios request

---
+++ src/app/store/store.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+import { QRUSHAPI } from '../../shared/API/api/api';
+
+export const usePages = create(set => ({
+  news: [],
+  getHomePage: async () => {
+    try {
+      const response = await QRUSHAPI.getHomePage();
+      set({ news: response.data });
+    } catch (error) {
+      console.error('Ошибка при получении страницы', error);
+    }
+  },
+}));
