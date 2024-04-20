951e613f35b4e0427b973fe63ff2c080fde82523
Kenjebai <Kenjebai>
2024-04-20 18:22:08 +0600

    finished all tasks i created backend and adaptive

--- src/widgets/aboutUsPageSections/introdactionSectionAboutUs/IntrodactionSectionAboutUs.jsx
+++ src/widgets/aboutUsPageSections/introdactionSectionAboutUs/IntrodactionSectionAboutUs.jsx
@@ -1,10 +1,7 @@
-// import { useTranslation } from 'react-i18next';
 import { useEffect } from 'react';
-// import woman from '../../../shared/img/aboutMobile/woman.png';
 import { useZustandStore } from '../../../app/store/store';
 
 export const IntrodactionSectionAboutUs = () => {
-  // const { t } = useTranslation();
   const { aboutUsTeam, getAboutUsTeam } = useZustandStore();
   useEffect(() => {
     getAboutUsTeam();
@@ -20,25 +17,16 @@ export const IntrodactionSectionAboutUs = () => {
         </div>
         <div className='sm:w-full flex gap-2'>
           <div className='flex sm:hidden h-[760px] pt-36 sm:pt-0'>
-            {/* <img src={woman} alt='woman' className='w-[131px] h-[179px]' /> */}
             <div>
               <h2 className='sm:max-w-[70%] pt-2 text-2xl font-regular lg:font-regular lg:text-7xl lg:text-[#F5F5F5] lg:pt-14 lg:leading-[100px] xl:pt-16 xl:text-[#E2DED3] xl:font-medium xl:text-8xl xl:leading-[122px]'>
-                {/* {t('aboutUs.firstSection.title')} */}
                 {aboutUsTeam[0]?.title}
               </h2>
-              {/* FIX ME: watch me */}
               <p className='leading-5 text-[#E2DED3] font-medium text-sm xl:text-2xl xl:leading-10'>
-                {/* {t('aboutUs.firstSection.upperTitle')} */}
                 {aboutUsTeam[0]?.description}
               </p>
             </div>
           </div>
         </div>
-        {/* <div className='sm:hidden md:hidden'>
-          <p className='max-w-[355px] pb-[3%] leading-5 text-[#E2DED3] font-medium text-sm xl:leading-10'>
-            {t('aboutUs.firstSection.upperTitle_2')}
-          </p>
-        </div> */}
       </div>
     </section>
   );
--- src/widgets/aboutUsPageSections/teamSection/cardTeamSection/CardTeamSection.jsx
+++ src/widgets/aboutUsPageSections/teamSection/cardTeamSection/CardTeamSection.jsx
@@ -1,37 +1,13 @@
 export const CardTeamSection = ({ item }) => {
+  const urlForImages = import.meta.env.VITE_IMG_URL;
   return (
     <li className='flex-row ml-[3%] mr-[3%] tablet:ml-0 tablet:mr-0 gap-2 pb-[2%] border-[#F93822] border-2 rounded-xl tablet:items-start p-2 mb-[50px] tablet:mb-[120px] xl:my-[6%] border-solid sm:border-transparent sm:flex-row flex sm:pt-2 sm:gap-8 md:flex-col lg:flex'>
-      {/* <div className='text-center max-w-[575px] mb-[10%] sm:max-w-[99%] tablet:block sm:justify-items-center sm:text-center md:max-w-[700px] tablet:max-w-[1000px] lg:max-w-[1300px] xl:max-w-[520px] xl:h-[650px] mx-auto'>
-        <img
-          src={item.image}
-          alt='photos_1'
-          className='max-w-[99%] mx-auto md:w-[700px] tablet:w-[1000px] lg:w-[1250px] xl:w-[520px] xl:h-[650px] object-cover'
-        />
-        <h3 className='text-[#F93822] font-bold text-xl sm:text-4xl py-7'>
-          {item.first_name_ru}
-        </h3>
-        <p className='font-medium text-[#E2DED3] text-base sm:text-3xl tablet:max-w-full'>
-          {item.role_ru}
-        </p>
-      </div> */}
-      {/* <div className='p-2 text-center max-w-[575px] mb-[10%] sm:max-w-[750px] md:w-[700px]  tablet:block sm:justify-items-center sm:text-center tablet:h-[650px] tablet:max-w-[450px] lg:max-w-[600px] xl:max-w-[520px] xl:h-[650px] mx-auto px-auto'>
-        <img
-          src={item.img}
-          alt='photos_1'
-          className='w-[570px] xl:rounded-2xl mx-auto sm:w-[750px] md:w-[700px] tablet:max-w-[450px] lg:max-w-[600px] xl:w-[520px] xl:h-[650px] object-cover'
-        />
-        <h3 className='text-[#F93822] font-bold text-xl sm:text-4xl py-7'>
-          {item.textName}
-        </h3>
-        <p className='font-medium max-w-full md:max-w-full text-[#E2DED3] text-base sm:text-3xl tablet:max-w-full'>
-          {item.text}
-        </p>
-      </div> */}
       <div className='p-2 text-center max-w-[575px] mb-[10%] sm:max-w-[750px] md:w-[700px]  tablet:block sm:justify-items-center sm:text-center tablet:h-[650px] tablet:max-w-[450px] lg:max-w-[600px] xl:max-w-[520px] xl:h-[650px] mx-auto px-auto'>
         <img
-          src={item.image}
+          // eslint-disable-next-line no-undef
+          src={`${urlForImages}${item.image}`}
           alt='photos_1'
-          className='w-[570px] xl:rounded-2xl mx-auto sm:w-[750px] md:w-[700px] tablet:max-w-[450px] lg:max-w-[600px] xl:w-[520px] xl:h-[650px] object-cover'
+          className='rounded-2xl w-[570px] max-h-[650px] h-[650px] xl:rounded-2xl mx-auto sm:w-[750px] md:w-[700px] tablet:max-w-[450px] lg:max-w-[600px] xl:w-[520px] xl:h-[650px] object-cover'
         />
         <h3 className='text-[#F93822] font-bold text-xl sm:text-4xl py-7'>
           {item.first_name}
